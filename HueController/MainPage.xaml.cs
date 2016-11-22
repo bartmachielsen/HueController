0daacea51c3914d5e5456a7dfd3ea90e0a1fbb02
nards <nard.swolfs@hotmail.com>
2016-11-22 14:55:28 +0100

    added option page and navigation system

--- HueController/MainPage.xaml.cs
+++ HueController/MainPage.xaml.cs
@@ -68,5 +68,15 @@ namespace HueController
             var button = ((Button)sender);
             button.Foreground = new SolidColorBrush(new Color() {A = (byte)random.Next(255), B = (byte)random.Next(255), G = (byte)random.Next(255), R = 255});
         }
+
+        private void HomepageClick(object sender, RoutedEventArgs e)
+        {
+            Frame.Navigate(typeof(MainPage));
+        }
+
+        private void SettingsClick(object sender, RoutedEventArgs e)
+        {
+            Frame.Navigate(typeof(SettingsPage));
+        }
     }
 }
