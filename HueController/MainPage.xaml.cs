dfa20a89537db0e2c36f257451540fbc303ea9d7
Bart Machielsen <bartmachielsen@gmail.com>
2016-11-22 10:08:40 +0100

    added base data binding and light model

--- HueController/MainPage.xaml.cs
+++ HueController/MainPage.xaml.cs
@@ -12,6 +12,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using HueController.Models;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -22,14 +23,24 @@ namespace HueController
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        private List<Light> lights = new List<Light>();
         public MainPage()
         {
+            this.lights = new List<Light>() { new Light(0), new Light(1)};
             this.InitializeComponent();
+            
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             SplitView.IsPaneOpen = !SplitView.IsPaneOpen;
+        }
+        
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            var button = ((Button) sender);
+            button.Content = (String)button.Content == "ON" ? "OFF" : "ON";
         }
     }
 }
---
+++ HueController/Models/Light.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HueController.Models
+{
+    public class Light
+    {
+        public bool state = false;
+        public int id;
+        public string name;
+
+        public string showtext
+        {
+
+            get
+            {
+                if (state)
+                    return "OFF";
+                return "ON";
+
+            }
+            set { state = "ON" == value; }
+
+        }
+
+        public Light(int id)
+        {
+            this.id = id;
+            this.name = "Light" + id;
+        }
+
+        public override string ToString()
+        {
+            return $"{name}, {id}, {state}";
+        }
+    }
+}
