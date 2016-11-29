de2d0d7dae0c38a358c1f496a2d92bb9467ee35b
Bart Machielsen <bartmachielsen@gmail.com>
2016-11-29 13:36:22 +0100

    added AnimationHandler

---
+++ HueController/Models/Animations/AnimationHandler.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HueController.Models.Animations
+{
+    class AnimationHandler
+    {
+        public static async void ExecuteAnimation(Animation animation, ObservableCollection<Light> lights )
+        {
+            for (int i = 0; i < animation.length; i++)
+            {
+                foreach (var light in lights)
+                {
+                    animation.ExecuteOne(i, light);
+                }
+                animation.RoundFinished();
+            }
+        }
+    }
+}
